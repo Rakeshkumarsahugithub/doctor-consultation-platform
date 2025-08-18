957c711323857f89e890b7686ecb5ed4dfc65b88
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-08-18 16:40:13 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: 'http://localhost:5000'
+    NEXT_PUBLIC_API_URL: 'https://doctor-consultation-platform.vercel.app'
   },
   images: {
     domains: ['images.unsplash.com', 'static.oxinis.com', 'media.istockphoto.com', 't4.ftcdn.net', 'www.kyd.co.in', 'via.placeholder.com', 'i.pinimg.com'],
