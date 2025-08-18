186a01084870632f3f5ad6e00d6b7e23463afe84
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-08-18 17:36:43 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'images.unsplash.com',
-      'static.oxinis.com',
-      'media.istockphoto.com',
-      't4.ftcdn.net',
-      'www.kyd.co.in',
-      'via.placeholder.com',
-      'i.pinimg.com',
-    ],
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
   },
-};
+  images: {
+    domains: ['images.unsplash.com', 'static.oxinis.com', 'media.istockphoto.com', 't4.ftcdn.net', 'www.kyd.co.in', 'via.placeholder.com', 'i.pinimg.com'],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+
 
 
