cd33b1910dd5591bc04a0b3992dce69fadfb2764
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-08-18 19:16:10 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: 'http://localhost:5000'
+     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://10.139.241.113:5000'
   },
   images: {
     domains: ['images.unsplash.com', 'static.oxinis.com', 'media.istockphoto.com', 't4.ftcdn.net', 'www.kyd.co.in', 'via.placeholder.com', 'i.pinimg.com'],
